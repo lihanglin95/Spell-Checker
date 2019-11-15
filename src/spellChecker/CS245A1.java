de60574e5a7145deccdec366f2ddb7727a8d52b6
lihanglin95 <37127371+lihanglin95@users.noreply.github.com>
2019-11-14 22:53:08 -0800

    2

--- src/spellChecker/CS245A1.java
+++ src/spellChecker/CS245A1.java
@@ -1,10 +1,12 @@
 package spellChecker;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.Properties;
 
 public class CS245A1 {
@@ -23,27 +25,50 @@ public class CS245A1 {
 			
 		test.creatDictionary();
 		test.checkInput(input);
-		test.printOuput();
+		//test.printOuput();
 	}
 	
-	public void printOuput() {
+	public void printOutput(String word) {
+		try(BufferedWriter bw = new BufferedWriter(new FileWriter("output.txt"))){
+			bw.write(word);
+			bw.newLine();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public void printOutput(String[] suggestions) {
 		// TODO Auto-generated method stub
 		
 	}
 
 	public void checkInput(String input) {
 		String thisLine = null;
-				
-		try(BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(input)));)
-		{	         
-	        while ((thisLine = br.readLine()) != null) {
-	        	dataStructure.find(thisLine);
-	            //System.out.println(thisLine);
-	        }       
-	    } catch(Exception e) {
-	         e.printStackTrace();
-	    }
+		boolean findWord = true;
+		
+		try(BufferedWriter bw = new BufferedWriter(new FileWriter("output.txt"))){
+			try(BufferedReader br = new BufferedReader(new FileReader("input.txt")))
+			{	         
+				while ((thisLine = br.readLine()) != null) {
+					//dataStructure.find(thisLine)
+					if(findWord) {
+						bw.write(thisLine);
+						bw.newLine();
+					}else
+						printOutput(dataStructure.suggest(thisLine));
+					//System.out.println(thisLine);
+				}       
+			} catch(Exception e) {
+				e.printStackTrace();
+			}
 
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+				
+		
 		
 	}
 
@@ -70,18 +95,16 @@ public class CS245A1 {
 		if(outputFile.createNewFile())
 			System.out.println("Create new output file successfullly.\n");
 		else
-			System.out.printf(output," is already exisit.\n");
+			System.out.println("The output file is already exisit.\n");
 		
 	}
 	
 	public void creatDictionary() {
 		String thisLine = null;
-		try {
+		try( BufferedReader br = new BufferedReader(new FileReader("english.0")))
+		{
 			
-	        // open input stream input.txt for reading purpose.
-			FileInputStream fStream = new FileInputStream("english.0");
-	        BufferedReader br = new BufferedReader(new InputStreamReader(fStream));
-	         
+	        // open input stream input.txt for reading purpose.    
 	        while ((thisLine = br.readLine()) != null) {
 	        	dataStructure.insert(thisLine);
 	            //System.out.println(thisLine);
